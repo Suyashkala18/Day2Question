55c8e775c45fbcd831b483d57510f83f40126e71
Suyashkala18 <suyash1.kala@lntinfotech.com>
2020-12-16 16:35:51 +0530

    Done by suyash kala

---
+++ FindPrimeSolution/FindPrime/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindPrime
+{
+    class Program
+    {
+        static bool findPrime(int num)
+        {
+            for (int i = 2; i <= num/2; i++)
+            {
+                if(num%i==0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the min number");
+            int min = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the max number");
+            int max = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = min; i <=max; i++)
+            {
+                if (findPrime(i))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
